e1204d8ba17f466a5fd115b060d062707d373071
Pantoush <jorgear.pantoja@gmail.com>
2019-04-10 14:32:36 -0500

    First version fixed

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setinv(inv)
+  inv
+}
